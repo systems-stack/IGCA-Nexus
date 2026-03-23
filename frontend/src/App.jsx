423f5d9a9b761f183a0d94d97c868af1dd03e5e2
systems-stack <systems@rohitkcjain.com>
2026-03-23 13:05:05 +0530

    Added HashRouter in the Main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import ParticleBackground from './components/ParticleBackground';
 import { Toaster } from 'react-hot-toast';
